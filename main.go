cd5eb4b0c26cb4489e8ae75825cc647af8dc3c2f
Evan Lewis <lewis.m.evan@gmail.com>
2021-08-13 09:45:54 -0500

    Add custom not found page

--- main.go
+++ main.go
@@ -17,8 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<p>Sorry, but we could not find the page you were looking for.</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	http.ListenAndServe("localhost:3000", r)
